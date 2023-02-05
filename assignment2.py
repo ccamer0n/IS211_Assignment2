d64ceae4b65ea110fadeb72c9c24d2673fecc95a
ccamer0n <christopher.cameron77@spsmail.cuny.edu>
2023-02-04 20:35:37 -0500

    Initial commit for work in progress

--- assignment2.py
+++ assignment2.py
@@ -3,24 +3,66 @@ import urllib.request
 import logging
 import datetime
 
+url = 'https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv'
 def downloadData(url):
     """Downloads the data"""
-    pass
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+    return the_page
 
+def error_logger(count, id_num):
+    LOG_FILENAME = 'errors.log'
+    logging.basicConfig(filename=LOG_FILENAME, level=logging.ERROR)
+    assignment2 = logging.getLogger('assignment2')
+    assignment2.error(f'Error processing line #{count} for ID #{id_num}.')
 def processData(file_content):
-    pass
+    person_list = file_content.decode('ascii').split('\n')
+    #LOG_FILENAME = 'errors.log'
+
+    #logging.basicConfig(filename = LOG_FILENAME, level = logging.ERROR)
+    #assignment2 = logging.getLogger('assignment2')
 
 
-def displayPerson(id, personData):
-    pass
+    big_dict = {}
+    count = 0
+    header = True
+    for person in person_list:
+        if header:
+            header = False
+            continue
+        if len(person) == 0:
+            continue
+        line = person.split(',')
+        id_num = int(line[0])
+        name = line[1]
 
-def main(url):
-    print(f"Running main with URL = {url}...")
+        birth_date = line[2]
+        count += 1
+        try:
+            format = '%d/%m/%Y'
+            bd = datetime.datetime.strptime(birth_date, format)
+        except Exception as err:
+            #assignment2.error(f'Error processing line #{count} for ID #{id_num}.')
+            error_logger(count, id_num)
+        big_dict[id_num] = (name, bd)
+    print(big_dict)
 
 
-if __name__ == "__main__":
-    """Main entry point"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
-    args = parser.parse_args()
-    main(args.url)
+
+processData(downloadData(url))
+
+
+#def displayPerson(id, personData):
+#    pass
+
+#def main(url):
+#    print(f"Running main with URL = {url}...")
+
+
+#if __name__ == "__main__":
+#    """Main entry point"""
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
+#    args = parser.parse_args()
+#    main(args.url)
